9a4c464336cbfe8fc526f6ed78ac07dea4d6863c
Martin Birkenberger <martin.birkenberger@insirion.com>
2024-04-02 18:26:17 +0200

    Add Pokemon API endpoint to HomeController

--- src/CargoCarrier.Mvc/Controllers/HomeController.cs
+++ src/CargoCarrier.Mvc/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CargoCarrier.Mvc.Models;
 using CargoCarrier.Mvc.Database;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace CargoCarrier.Mvc.Controllers;
 
@@ -9,8 +11,9 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly TripDatabase _tripDatabase;
+    private static readonly HttpClient httpClient = new();
 
-    public HomeController(ILogger<HomeController> logger,  TripDatabase tripDatabase)
+    public HomeController(ILogger<HomeController> logger, TripDatabase tripDatabase)
     {
         _logger = logger;
         _tripDatabase = tripDatabase;
@@ -42,6 +45,42 @@ public class HomeController : Controller
     {
         var trips = _tripDatabase.GetTripsWithParcelSizeLessThanAsync(maxParcelSize);
         return trips;
+    }
+
+    public async Task<IActionResult> Pokemon()
+    {
+        try
+        {
+            // Generate a random number between 1 and 898
+            var randomId = new Random().Next(1, 899);
+
+            // Construct the URL for the API call
+            var url = $"https://pokeapi.co/api/v2/pokemon/{randomId}";
+
+            // Make the GET request
+            var response = await httpClient.GetAsync(url);
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the JSON response
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var pokemonData = JsonSerializer.Deserialize<JsonNode>(responseBody);
+
+                // Extract and return the name of the Pokemon
+                return Ok(pokemonData?["name"]?.ToString());
+            }
+            else
+            {
+                // Handle the error
+                return StatusCode((int)response.StatusCode, $"Failed to retrieve Pokemon data. Status code: {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any other exceptions
+            return BadRequest($"An error occurred: {ex.Message}");
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
